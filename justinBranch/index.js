9f5c88e28cc20dde75707043d55829ad01f82c88
Justin Siebert <19justinsiebert99@gmail.com>
2026-05-17 16:13:48 +0200

    changes page justin

--- justinBranch/index.js
+++ justinBranch/index.js
@@ -1,15 +1,5 @@
 const participantNameH1 = document.querySelector("#participantName")
 
-let pID = 148;
-
-function getParticipantName (id){
-    let participantName = participants.find((d) =>  d.id == pID) // objeket
-
-    return participantName
-}
-
-participantNameH1.textContent = `Name : ${getParticipantName(pID).name}`
-
 let statsPerYear = [
     // {year : number
     // trainer : id
@@ -17,44 +7,85 @@ let statsPerYear = [
     //}
 ];
 
-for (let season of seasons){
-    // {year: number},  {trainers: [{pID: number, tID: number}]}, {competitionDays: [{events: [{disciplin: number, scores: [{participantId: number, score: number}]}]}]}
+let allPlayerStats = []
 
-    let year = season.year
-    let idOftrainer = 0
-    let totScore = 0
-    let disciplinesIdsWScore = disciplines.map((d) => {
-        return {"discipline": `${d.id}`, "totsum": 0}
-    })
-    
-    
-    for (let trainer of season.trainers) {
-        if(trainer.participantId == pID){
-            idOftrainer = trainer.trainerId // tränare id
+for(let p of participants){
+    getPlayerStatsPerSeason(p.id)
+}
+
+let highestScoreAllP = 0;
+for (let player of allPlayerStats){
+    for (let i of player.stats){
+        for (let j of i.scorePerDiscpline){
+            highestScoreAllP = j.totsum > highestScoreAllP ? j.totsum : highestScoreAllP
         }
     }
+}
+
+console.log(allPlayerStats)
+console.log(highestScoreAllP)
+
+//let pID = getPlayerStatsPerSeason(125);
+
+// function getParticipantName (id){
+//     let participantName = participants.find((d) =>  d.id == pID) // objeket
+
+//     return participantName
+// }
+
+//participantNameH1.textContent = `Name : ${getParticipantName(pID).name}`
 
 
 
-    for (let competitionDay of season.competitionDays){
-        for (let event of competitionDay.events){
-            for (let x of event.scores){
-                if (x.participantId == pID){
-                    let index = disciplinesIdsWScore.findIndex((d) => d.discipline == event.disciplineId)
-                    disciplinesIdsWScore[index].totsum += x.score
+function getPlayerStatsPerSeason (id){
+    statsPerYear = []
+
+    for (let season of seasons){
+        // {year: number},  {trainers: [{pID: number, tID: number}]}, {competitionDays: [{events: [{disciplin: number, scores: [{participantId: number, score: number}]}]}]}
+
+        let year = season.year
+        let idOftrainer = 0
+        let totScore = 0
+        let disciplinesIdsWScore = disciplines.map((d) => {
+            return {"discipline": `${d.id}`, "totsum": 0}
+        })
+        
+        
+        for (let trainer of season.trainers) {
+            if(trainer.participantId == id){
+                idOftrainer = trainer.trainerId // tränare id
+            }
+        }
+
+
+
+        for (let competitionDay of season.competitionDays){
+            for (let event of competitionDay.events){
+                for (let x of event.scores){
+                    if (x.participantId == id){
+                        let index = disciplinesIdsWScore.findIndex((d) => d.discipline == event.disciplineId)
+                        disciplinesIdsWScore[index].totsum += x.score
+                    }
                 }
             }
         }
+        
+        statsPerYear.push({
+            "year": year,
+            "trainer": idOftrainer,
+            "scorePerDiscpline": disciplinesIdsWScore
+        })
+
+
     }
-    
-    statsPerYear.push({
-        "year": year,
-        "trainer": idOftrainer,
-        "scorePerDiscpline": disciplinesIdsWScore
+
+        
+    allPlayerStats.push({
+        "playerId": id,
+        "stats" : statsPerYear
     })
-
+        
 }
-
 console.log(statsPerYear)
 
 // ------------------- SVG ----------------------
